83396f20ad3b0c371a4b4d2eeb4b720b55d5f966
Luke <modularluke@gmail.com>
2021-04-17 21:53:14 +1000

    Testing sending and receiving ROS messages

---
+++ rostest.m
@@ -0,0 +1,47 @@
+clear; close;
+ip = '192.168.174.128';
+rosinit(ip,11311)
+rostopic list
+
+global pose; % to store most recent pose
+
+% define twist message to send
+twistmsg = rosmessage('geometry_msgs/Twist');
+twistmsg.Linear.X = 0.1;
+twistmsg.Linear.Y = 0;
+twistmsg.Angular.Z = 1;
+
+% define subscriber and publisher
+odomsub = rossubscriber('/odom',@OdomCallback);
+velpub = rospublisher('/cmd_vel','geometry_msgs/Twist');
+
+% create timer to send twist messages
+t = timer;
+t.ExecutionMode = 'fixedRate';
+t.TimerFcn = 'send(velpub,twistmsg)';
+t.Period = 0.1;
+start(t);
+
+poses = [];
+for i = 1:10
+    poses = [poses;pose];
+    pause(1);
+end
+
+twistmsg.Angular.Z = -1;
+
+for i = 1:10
+    poses = [poses;pose];
+    pause(1);
+end
+
+stop(t);
+delete(t);
+rosshutdown;
+
+plot(poses(:,1),poses(:,2));
+
+function OdomCallback(src,msg)
+global pose;
+pose = OdometryTo2DPose(msg);
+end
