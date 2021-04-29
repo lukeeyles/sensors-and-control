7346fd0bc1da660d793a26a40fcab91762935f3f
Luke <modularluke@gmail.com>
2021-04-29 16:48:45 +1000

    goal 1 direction perpendicular to normal

--- Localisation/FindGoal.m
+++ Localisation/FindGoal.m
@@ -2,8 +2,8 @@
 % Parameters :  squarecentre - location of square [x,y]
 %               squarenormal - normal of the square [x,y]
 %               odom - robot odometry message (nav_msgs/Odometry)
-% Return:       goal1 - first goal [x,y,theta]
-%               goal2 - second goal [x,y,theta]
+% Return:       goal1 - first goal facing perpendicular to normal [x,y,theta]
+%               goal2 - second goal facing parallel to normal [x,y,theta]
 function [goal1, goal2] = FindGoal(squarecentre, squarenormal, odom)
 % goal 1 at the perpendicular intersection of robot and square normal
 % solve linear eqns to find perpendicular intersection
@@ -13,8 +13,10 @@ A = [-squarenormal(2) -squarenormal(1);
     squarenormal(1) -squarenormal(2)];
 X = linsolve(A,b);
 intersection = squarecentre + X(2)*squarenormal;
-direction = cart2pol(-squarenormal(1),-squarenormal(2));
-goal1 = [intersection, direction];
+diff = intersection - [odom.Pose.Pose.Position.X odom.Pose.Pose.Position.Y];
+direction1 = atan2(diff(2),diff(1));
+goal1 = [intersection, direction1];
 
 % goal 2 at the square - project out 20cm
-goal2 = [squarecentre+0.2*squarenormal, direction];
+direction2 = cart2pol(-squarenormal(1),-squarenormal(2));
+goal2 = [squarecentre+0.2*squarenormal, direction2];
