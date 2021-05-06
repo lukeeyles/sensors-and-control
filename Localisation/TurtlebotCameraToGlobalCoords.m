9e6f613c00dc372369948eb7db7d5a52ac906cad
Luke <modularluke@gmail.com>
2021-05-06 15:16:15 +1000

    return row vector

--- Localisation/TurtlebotCameraToGlobalCoords.m
+++ Localisation/TurtlebotCameraToGlobalCoords.m
@@ -3,7 +3,7 @@
 % Parameters :  cameracoords - [x, y, z] coords in camera reference frame
 %               odom - robot odometry message
 %               cameraheight - height of camera on turtlebot
-% Return :      globalcoords - [x; y; z] coords in global reference frame
+% Return :      globalcoords - [x, y, z] coords in global reference frame
 function globalcoords = TurtlebotCameraToGlobalCoords(cameracoords, odom, cameraheight)
 RobotRotation = 2*acos(odom.Pose.Pose.Orientation.W);
 ObjectCameraT = transl(cameracoords);
@@ -11,4 +11,4 @@ CameraRobotT = transl([0 0 cameraheight])*trotx(pi/2)*troty(pi/2);
 RobotGlobalT = transl(odom.Pose.Pose.Position.X, odom.Pose.Pose.Position.Y, 0)*...
     trotz(RobotRotation);
 ObjectGlobalT = RobotGlobalT*CameraRobotT*ObjectCameraT;
-globalcoords = ObjectGlobalT(1:3,4);
+globalcoords = ObjectGlobalT(1:3,4)';
