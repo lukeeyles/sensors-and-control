820621aed377d9dc463c975e2473c12bc65e2537
Luke <modularluke@gmail.com>
2021-05-09 17:03:30 +1000

    no message

--- Localisation/MakeCalibrationMatrix.m
+++
@@ -1,4 +0,0 @@
-function K = MakeCalibrationMatrix(focallength, pixelsize, principlepoint)
-K = [focallength/pixelsize(1) 0 principlepoint(1);
-    0 focallength/pixelsize(2) principlepoint(2);
-    0 0 1];
