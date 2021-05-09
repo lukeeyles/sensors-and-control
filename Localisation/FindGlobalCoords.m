53642771f462a7ecc06cceac55392372e68027d4
Luke <modularluke@gmail.com>
2021-05-09 17:52:48 +1000

    bug fix

--- Localisation/FindGlobalCoords.m
+++ Localisation/FindGlobalCoords.m
@@ -3,17 +3,15 @@
 %               rgbimg - rgb image the card was located in
 %               depthimg - depth image at the same time
 %               odom - turtlebot odometry
-%               calibrationMatrix - camera calibration matrix constructed
-%               with MakeCalibrationMatrix
+%               calibrationMatrix - camera calibration matrix
 % Return :      globalCoords [x1 y1 z1; x2 y2 z2] of card points
 function globalCoords = FindGlobalCoords(cardloc,rgbimg,depthimg,odom,calibrationMatrix)
-cameraheight = 0.15;
-depthidx = size(depthimg).*cardloc./size(rgbimg); % resize coords so we can find the same point on the depth image
-depth = [depthimg(depthidx(1,:)); depthimg(depthidx(2,:))];
+depthidx = round(size(depthimg).*cardloc./size(rgbimg,1,2)); % resize coords so we can find the same point on the depth image
+depth = [depthimg(depthidx(1,1),depthidx(1,2)); depthimg(depthidx(2,1),depthidx(2,2))];
 % TODO transform depth to actual depth in m....
 
 globalCoords = zeros(2,3);
 for i = 1:size(depth,1)
-    cameracoords = PixelToCameraCoords(cardloc(i,:), depth(i,:), calibrationMatrix);
-    globalCoords(i,:) = TurtlebotCameraToGlobalCoords(cameracoords, odom, cameraheight);
+    cameracoords = PixelToCameraCoords(cardloc(i,:), depth(i), calibrationMatrix);
+    globalCoords(i,:) = TurtlebotCameraToGlobalCoords(cameracoords', odom);
 end
