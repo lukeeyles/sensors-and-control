bd3f849e517832fd408d61346537bca1906940b6
Luke <modularluke@gmail.com>
2021-05-09 17:02:50 +1000

    fixed turtlebot to camera transform

--- Localisation/TurtlebotCameraToGlobalCoords.m
+++ Localisation/TurtlebotCameraToGlobalCoords.m
@@ -6,9 +6,9 @@
 % Return :      globalcoords - [x, y, z] coords in global reference frame
 function globalcoords = TurtlebotCameraToGlobalCoords(cameracoords, odom, cameraheight)
 RobotRotation = 2*acos(odom.Pose.Pose.Orientation.W);
-ObjectCameraT = transl(cameracoords);
-CameraRobotT = transl([0 0 cameraheight])*trotx(pi/2)*troty(pi/2);
-RobotGlobalT = transl(odom.Pose.Pose.Position.X, odom.Pose.Pose.Position.Y, 0)*...
+T_OC = transl(cameracoords);
+T_CR = transl([0 0 cameraheight])*trotx(-pi/2)*troty(pi/2);
+T_RG = transl(odom.Pose.Pose.Position.X, odom.Pose.Pose.Position.Y, 0)*...
     trotz(RobotRotation);
-ObjectGlobalT = RobotGlobalT*CameraRobotT*ObjectCameraT;
-globalcoords = ObjectGlobalT(1:3,4)';
+T_OG = T_RG*T_CR*T_OC;
+globalcoords = T_OG(1:3,4)';
