da451b249f6539f63a8f894903d6aed55b4738d7
Luke <modularluke@gmail.com>
2021-05-09 17:01:42 +1000

    comments

--- Localisation/PixelToCameraCoords.m
+++ Localisation/PixelToCameraCoords.m
@@ -1,10 +1,9 @@
 % Description : Converts pixel coords to coords in camera frame
 % Parameters :  pixelindex - [row, col] of pixel in image
-%               depth - depth at same location from depth image
-%               calibrationMatrix - camera calibration matrix constructed
-%               with MakeCalibrationMatrix
-% Return:       localcoords - 3D coords of point in camera frame
+%               depth - depth at same location from depth image (m)
+%               calibrationMatrix - camera calibration matrix
+% Return:       cameracoords - 3D coords of point in camera frame
 function cameracoords = PixelToCameraCoords(pixelindex, depth, calibrationMatrix)
 pixelcoords = [pixelindex(2); pixelindex(1); 1];
-pixelhomog = pixelcoords*depth;
+pixelhomog = pixelcoords.*depth;
 cameracoords = inv(calibrationMatrix)*pixelhomog;
