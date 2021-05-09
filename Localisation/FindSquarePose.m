b44742d5b7d5c864440c0d3799c947b5c960d8a4
Luke <modularluke@gmail.com>
2021-05-09 17:56:46 +1000

    scale normal to unit vector

--- Localisation/FindSquarePose.m
+++ Localisation/FindSquarePose.m
@@ -1,4 +1,6 @@
 % Description : find 2d normal and centre of square from global coords
-function [normal,centre] = FindSquarePose(globalCoords,odom)
+function [normal,centre] = FindSquarePose(globalCoords)
 % want to point the normal towards the robot
 centre = mean(globalCoords(:,1:2));
+parallel = globalCoords(2,1:2) - globalCoords(1,1:2);
+normal = [parallel(2) -parallel(1)]./sqrt(parallel(1)^2 + parallel(2)^2);
