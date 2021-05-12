b4536dff2e59ccb2c2b9a82353f0bb2201774d4a
Luke <modularluke@gmail.com>
2021-05-12 16:18:30 +1000

    break when marker is found

--- main.m
+++ main.m
@@ -1,5 +1,6 @@
 clear; close;
 ip = '192.168.0.20';
+rosshutdown;
 
 % intialise ros if it isn't initialised
 try
@@ -8,7 +9,7 @@ catch exp
     rosinit(ip,11311,'NodeHost','192.168.0.2')
 end
 
-
+%%
 % subscribers and publishers
 rgbsub = rossubscriber('/camera/color/image_raw');
 depthsub = rossubscriber('/camera/depth/image_raw');
@@ -16,7 +17,7 @@ odomsub = rossubscriber('/odom',@OdomCallback);
 velpub = rospublisher('/cmd_vel','geometry_msgs/Twist');
 global odomGlobal; % to store most recent odometry
 global twistmsg;    % to store most recent velocities
-%%
+
 % define starting twist message to send
 twistmsg = rosmessage('geometry_msgs/Twist');
 
@@ -28,8 +29,9 @@ t.Period = 0.1;
 start(t);
 
 % main loop
-numcards = 3; % number of cards to read
-while numcards > 0
+markernames = ["marker_1.jpg","marker_2.jpg","marker_3.jpg"];
+markernames = ["marker_1.jpg"];
+for i = 1:numel(markernames)
     qrloc = [];
     depth = [];
     rgb = [];
@@ -37,6 +39,7 @@ while numcards > 0
     % look for marker
     angleIncrement = pi/8;
     for angle = -pi:angleIncrement:pi
+        fprintf("Driving to angle: %d",rad2deg(angle));
         DriveToAngle(angle,odomsub);
         
         % read depth and rgb images, convert to matlab format
@@ -44,13 +47,19 @@ while numcards > 0
         rgbmsg = receive(rgbsub,2);
         depth = readImage(depthmsg);
         rgb = readImage(rgbmsg);
+        disp("Received images");
         
-        % look for playing card in rgb image
-        [card1,card2] = FindCardLocation(numcards); % TODO find location of 2 points on card
+        % look for marker in rgb image
+        [point1,point2] = MarkerDetection(markernames(i),rgb); % find location of 2 points on marker
+        if (~all(point1==0) && ~all(point2==0))
+            fprintf("Found marker at: %d, %d",point1(1),point1(2));
+            break
+        end
     end
     
     % find global 3D coords of card
-    globalCoords = FindGlobalCoords([card1;card2],rgb,depth,odomGlobal);
+    globalCoords = FindGlobalCoords([point1;point2],rgb,depth,odomGlobal);
+    fprintf("Marker at: %d, %d",globalCoords(1),globalCoords(2));
     
     % find normal and centre of card
     [normal,centre] = FindSquarePose(globalCoords(:,1:2));
