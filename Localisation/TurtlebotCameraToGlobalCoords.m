0319d3d5b66d6fb93c680a35cd8769a289a2db93
Luke <modularluke@gmail.com>
2021-05-19 13:28:13 +1000

    fix robot rotation

--- Localisation/TurtlebotCameraToGlobalCoords.m
+++ Localisation/TurtlebotCameraToGlobalCoords.m
@@ -5,9 +5,14 @@
 % Return :      globalcoords - [x, y, z] coords in global reference frame
 function globalcoords = TurtlebotCameraToGlobalCoords(cameracoords, odom)
 cameraheight = 0.1;
-RobotRotation = 2*acos(odom.Pose.Pose.Orientation.W);
+%RobotRotation = 2*acos(odom.Pose.Pose.Orientation.W);
+RobotRotation = odom.Pose.Pose.Orientation;
+RobotRotation = quat2eul([RobotRotation.W,RobotRotation.X,RobotRotation.Y,RobotRotation.Z]);
+RobotRotation = RobotRotation(1);
+
 T_OC = transl(cameracoords);
-T_CR = transl([0.1 0 cameraheight])*trotx(-pi/2)*troty(pi/2);
+T_CR = transl([0.1 -0.01 cameraheight])*trotx(-pi/2)*troty(pi/2);
+%T_CR = transl([0 0 0])*trotx(-pi/2)*troty(pi/2);
 T_RG = transl(odom.Pose.Pose.Position.X, odom.Pose.Pose.Position.Y, 0)*...
     trotz(RobotRotation);
 T_OG = T_RG*T_CR*T_OC;
