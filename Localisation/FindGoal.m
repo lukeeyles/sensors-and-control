d2d262223180c73a310a5d59f6cf49ed1263c584
Luke <modularluke@gmail.com>
2021-05-19 13:28:30 +1000

    add offset from marker as input

--- Localisation/FindGoal.m
+++ Localisation/FindGoal.m
@@ -4,7 +4,7 @@
 %               odom - robot odometry message (nav_msgs/Odometry)
 % Return:       goal1 - first goal facing perpendicular to normal [x,y,theta]
 %               goal2 - second goal facing parallel to normal [x,y,theta]
-function [goal1, goal2] = FindGoal(squarecentre, squarenormal, odom)
+function [goal1, goal2] = FindGoal(squarecentre, squarenormal, odom, offset)
 % goal 1 at the perpendicular intersection of robot and square normal
 % solve linear eqns to find perpendicular intersection
 b = [squarecentre(1) - odom.Pose.Pose.Position.X;
@@ -20,7 +20,7 @@ goal1 = [intersection, direction1];
 % goal 2 at the square - project out 20cm
 direction2 = cart2pol(-squarenormal(1),-squarenormal(2));
 if X(2) > 0 % normal faces robot
-    goal2 = [squarecentre+0.2*squarenormal, direction2];
+    goal2 = [squarecentre+offset*squarenormal, direction2];
 else % normal faces away from robot
-    goal2 = [squarecentre-0.2*squarenormal, direction2];
+    goal2 = [squarecentre-offset*squarenormal, direction2];
 end
