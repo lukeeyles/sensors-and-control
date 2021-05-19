63bb9f8f25725c1fa00ed25bfded5d051931b9ae
Luke <modularluke@gmail.com>
2021-05-19 13:09:42 +1000

    use regions to match depth image with rgb image

--- Localisation/FindGlobalCoords.m
+++ Localisation/FindGlobalCoords.m
@@ -7,10 +7,24 @@
 % Return :      globalCoords [x1 y1 z1; x2 y2 z2] of card points
 function globalCoords = FindGlobalCoords(cardloc,rgbimg,depthimg,odom,calibrationMatrix)
 cardloc = fliplr(cardloc); % convert from x,y to row,col
-depthcrop = [43.5100000000000,8.51000000000000,436.980000000000,351.980000000000]
+depthcrop = [40,0.5100,445.9800,359.9800];
 depthimg = imcrop(depthimg,depthcrop);
 
-depthidx = round(size(depthimg).*cardloc./size(rgbimg,1,2)) % resize coords so we can find the same point on the depth image
+meanpoint = mean(cardloc); % centre in rgb image
+leftpointscaled = round(size(depthimg).*cardloc(2,:)./size(rgbimg,1,2));
+rightpointscaled = round(size(depthimg).*cardloc(1,:)./size(rgbimg,1,2));
+meanpointscaled = round(size(depthimg).*meanpoint./size(rgbimg,1,2));
+centredistance = meanpointscaled(2) - leftpointscaled(2);
+
+% match centroid in depth image to centre in rgb image
+centroids = FindCentroids(depthimg)
+closestcentroid = FindClosestPoint(meanpointscaled,fliplr(centroids))
+depthidx(1,:) = closestcentroid + [0 centredistance];
+depthidx(2,:) = closestcentroid - [0 centredistance];
+depthidx = round(depthidx);
+depthidx(:,1) = clamp(depthidx(:,1),1,size(depthimg,1));
+depthidx(:,2) = clamp(depthidx(:,2),1,size(depthimg,2));
+depthidx
 
 figure(3)
 imagesc(depthimg);
@@ -19,6 +33,9 @@ hold on;
 depthr = depthimg(depthidx(1,1),depthidx(1,2));
 while depthr == 0
     % decrement column until we find something
+    if depthidx(1,2) < 1
+        break
+    end
     depthidx(1,2) = depthidx(1,2) - 1;
     depthr = depthimg(depthidx(1,1),depthidx(1,2));
     plot(depthidx(1,2),depthidx(1,1),'g*');
@@ -28,12 +45,15 @@ end
 depthl = depthimg(depthidx(2,1),depthidx(2,2));
 while depthl == 0
     % increment column until we find something
+    if depthidx(2,2) > size(depthimg,2)
+        break
+    end
     depthidx(2,2) = depthidx(2,2) + 1;
     depthl = depthimg(depthidx(2,1),depthidx(2,2));
     plot(depthidx(2,2),depthidx(2,1),'g*');
 end
 
-depth = [depthl; depthr]
+depth = [depthr; depthl]
 
 plot(depthidx(1,2),depthidx(1,1),'r*');
 plot(depthidx(2,2),depthidx(2,1),'r*');
