81851be414a739142af0d754fa6207a7c18bdf37
Luke <modularluke@gmail.com>
2021-05-20 10:57:55 +1000

    image and console output

--- Localisation/FindCentroids.m
+++ Localisation/FindCentroids.m
@@ -2,10 +2,16 @@ function centroid = FindCentroids(img,arearange)
 BW = img > 0;
 BW = bwareaopen(BW, 100);
 BW = ~bwareaopen(~BW, 100);
+figure(4);
+imshow(BW);
 
 [B,L] = bwboundaries(BW,'noholes');
 stats = regionprops(L,'ConvexArea','Centroid');
 centroid = cat(1,stats.Centroid);
-area = cat(1,stats.ConvexArea);
+area = cat(1,stats.ConvexArea)
 areamatch = (area > arearange(1) & area < arearange(2));
 centroid = centroid(areamatch,:);
+
+if size(centroid,1) == 0
+    disp("NO CENTROIDS FOUND");
+end
